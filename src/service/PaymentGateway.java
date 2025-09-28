f17ce9b2684a1edcf33fed159dc084beab83c85c
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-09-28 11:57:56 +0530

    code refactored with Bill Pugh Singleton pattern

--- src/service/PaymentGateway.java
+++ src/service/PaymentGateway.java
@@ -10,36 +10,33 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class PaymentGateway {
 
-    private static volatile PaymentGateway paymentGateway;
     private final Queue<Transaction> transactions;
     private PaymentGateway() {
         transactions = new ConcurrentLinkedQueue<>();
-    };
-
-    public static PaymentGateway getInstance() {
-        if(paymentGateway == null) {
-            synchronized (PaymentGateway.class) {
-                if(paymentGateway == null) {
-                    paymentGateway = new PaymentGateway();
-                }
-            }
-        }
-        return paymentGateway;
     }
 
-    public boolean addNewTransaction(User sender, User receiver, double amount, PaymentMethod method, String credential) {
-        this.transactions.add(new Transaction(sender, receiver, amount, method, credential));
+    private static class PaymentGatewayHelper {
+        private static final PaymentGateway INSTANCE = new PaymentGateway();
+    }
+    public static PaymentGateway getInstance() {
+        return PaymentGatewayHelper.INSTANCE;
+    }
+
+    public synchronized boolean createNewTransaction(User sender, User receiver, double amount, PaymentMethod method) {
+        this.transactions.add(new Transaction(sender, receiver, amount, method));
         return processTransaction();
     }
 
     private synchronized boolean processTransaction() {
         int retryAttempt = 3; // can be saved in Config
         Transaction transaction = transactions.poll();
+
         while(retryAttempt > 0) {
             if(!transaction.initiatePayment()) {
                 retryAttempt--;
             } else {
                 transaction.setTransactionStatus(TransactionStatus.SUCCESS);
+                return true;
             }
         }
 
